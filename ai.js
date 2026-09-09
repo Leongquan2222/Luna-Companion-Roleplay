94fc0f961cb67d79fedbcef24b312f4689644811
Leongquan2222 <leh933827@gmail.com>
2026-09-09 21:48:58 +0700

    Update ai.js

--- ai.js
+++ ai.js
@@ -202,7 +202,7 @@ document.addEventListener('DOMContentLoaded', () => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          model: 'command-r-plus-08-2024',
+          model: 'command-a-03-2025',
           preamble: sysPrompt,
           message: finalPrompt,
           chat_history: conversationHistory,
